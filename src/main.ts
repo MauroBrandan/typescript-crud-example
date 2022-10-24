14b2f668c167d3bde84dd2fff5422e0a3dbdc938
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-10-24 14:12:13 -0300

    Product Service: addProduct

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
+import { products, addProduct } from './products/product.services'
 
+addProduct({
+	id: '5',
+	description: 'description',
+	image: 'image',
+	color: 'color',
+	size: 'M',
+	price: 55,
+	isNew: true,
+	tags: ['tag1', 'tag2'],
+	title: 'Hola Mundo',
+	createdAt: new Date(),
+	updatedAt: new Date(),
+	stock: 13,
+	category: {
+		id: 'id',
+		name: 'name',
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+})
+
+console.log(products)
