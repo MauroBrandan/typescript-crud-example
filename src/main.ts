720a4abad7f7730ba27ff6f36372cb3a011b4407
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-10-25 12:35:05 -0300

    Add CreateProductDto

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { faker } from '@faker-js/faker'
-
 import { products, addProduct } from './products/product.services'
 
 for (let index = 0; index < 25; index++) {
 	addProduct({
-		id: faker.datatype.uuid(),
 		description: faker.commerce.productDescription(),
 		image: faker.image.imageUrl(),
 		color: faker.color.human(),
@@ -13,15 +11,8 @@ for (let index = 0; index < 25; index++) {
 		isNew: faker.datatype.boolean(),
 		tags: faker.helpers.arrayElements(),
 		title: faker.commerce.productName(),
-		createdAt: faker.date.recent(),
-		updatedAt: faker.date.recent(),
 		stock: faker.datatype.number({ min: 10, max: 100 }),
-		category: {
-			id: faker.datatype.uuid(),
-			name: faker.commerce.department(),
-			createdAt: faker.date.recent(),
-			updatedAt: faker.date.recent(),
-		},
+		categoryId: faker.datatype.uuid(),
 	})
 }
 
