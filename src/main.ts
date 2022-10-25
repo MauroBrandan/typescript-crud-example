9cb7587a1e7a8af2335643e07ffe8a64512bc54a
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-10-25 11:50:05 -0300

    Fakerjs

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
+import { faker } from '@faker-js/faker'
+
 import { products, addProduct } from './products/product.services'
 
-addProduct({
-	id: '5',
-	description: 'description',
-	image: 'image',
-	color: 'color',
-	size: 'M',
-	price: 55,
-	isNew: true,
-	tags: ['tag1', 'tag2'],
-	title: 'Hola Mundo',
-	createdAt: new Date(),
-	updatedAt: new Date(),
-	stock: 13,
-	category: {
-		id: 'id',
-		name: 'name',
-		createdAt: new Date(),
-		updatedAt: new Date(),
-	},
-})
+for (let index = 0; index < 25; index++) {
+	addProduct({
+		id: faker.datatype.uuid(),
+		description: faker.commerce.productDescription(),
+		image: faker.image.imageUrl(),
+		color: faker.color.human(),
+		size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
+		price: parseInt(faker.commerce.price(), 10),
+		isNew: faker.datatype.boolean(),
+		tags: faker.helpers.arrayElements(),
+		title: faker.commerce.productName(),
+		createdAt: faker.date.recent(),
+		updatedAt: faker.date.recent(),
+		stock: faker.datatype.number({ min: 10, max: 100 }),
+		category: {
+			id: faker.datatype.uuid(),
+			name: faker.commerce.department(),
+			createdAt: faker.date.recent(),
+			updatedAt: faker.date.recent(),
+		},
+	})
+}
 
 console.log(products)
